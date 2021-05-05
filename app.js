936813b935bee4b5084664a0acc9a3de061786bd
Missael Ortiz <ortiz.missael80@gmail.com>
2021-05-04 17:05:51 -0700

    added button and fixed logic

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 // import functions and grab DOM elements
 import { compareGuess } from './utils.js';
 const guessesRemainingDisplay = document.getElementById('guesses-remaining');
-const totalWinsDisplay = document.getElementById('toal-wins');
+const totalWinsDisplay = document.getElementById('total-wins');
 const totalPlaysDisplay = document.getElementById('total-plays');
 const userGuessInput = document.getElementById('user-guess');
 const submitGuessButton = document.getElementById('submit-guess');
@@ -12,10 +12,10 @@ const feedbackDisplay = document.getElementById('feedback');
 let totalWins = 0;
 let totalPlays = 0;
 let guessesRemaining = 4;
+let targetNumber = Math.ceil(Math.random() * 20);
 
 // set event listeners to update state and DOM
 submitGuessButton.addEventListener('click', () => {
-    let targetNumber = Math.ceil(Math.random() * 20);
     if (compareGuess(Number(userGuessInput.value), Number(targetNumber)) === +1) {
         guessesRemaining--;
         guessesRemainingDisplay.textContent = `${guessesRemaining} Guesses Remaining`;
@@ -30,13 +30,23 @@ submitGuessButton.addEventListener('click', () => {
         totalWinsDisplay.textContent = totalWins;
         totalPlays++;
         totalPlaysDisplay.textContent = totalPlays;
-        resetGameButton.classList.toggle = 'hide';
-        submitGuessButton.classList.toggle = 'hide';
+        resetGameButton.classList.toggle('hide');
+        userGuessInput.classList.toggle('hide');
+        submitGuessButton.classList.toggle('hide');
     } if (guessesRemaining === 0) {
-        resetGameButton.classList.toggle = 'hide';
-        submitGuessButton.classList.toggle = 'hide';
+        resetGameButton.classList.toggle('hide');
+        userGuessInput.classList.toggle('hide');
+        submitGuessButton.classList.toggle('hide');
         totalPlays++;
         totalPlaysDisplay.textContent = totalPlays;
         feedbackDisplay.textContent = 'Oh NO! The world blew up, but you can try again!';
     }
 });
+
+resetGameButton.addEventListener('click', () => {
+    resetGameButton.classList.toggle('hide');
+    userGuessInput.classList.toggle('hide');
+    submitGuessButton.classList.toggle('hide');
+    guessesRemaining = 4;
+    targetNumber = Math.ceil(Math.random() * 20);
+})
